18e4b75f038e1f0b6e55f3a0f7ea9726794d049e
unluckynilesh <nileshinde001@gmail.com>
2025-04-19 15:27:01 +0530

    server fixed 4

--- server/celery_config.py
+++ server/celery_config.py
@@ -2,18 +2,20 @@ from celery import Celery
 import os
 
 # Get Redis URL from environment variable or use a default
-redis_url = os.getenv("REDIS_URL")
+REDIS_URL = os.getenv("REDIS_URL")
 
 # Create Celery app
 celery_app = Celery(
     "reddit_story_app",
-    broker=redis_url,
-    backend=redis_url,
+    broker=REDIS_URL,
+    backend=REDIS_URL,
     include=['tasks.reddit_story_task']  # Include the task modules here
 )
 
 # Optional: Configure Celery with additional settings
 celery_app.conf.update(
+    task_time_limit=300,  # Give tasks up to 5 minutes
+    task_soft_time_limit=240 ,
     result_expires=3600,  # Results expire after 1 hour
     task_serializer='json',
     accept_content=['json'],
