0c440641db40caba80dcf8fbe5f05fd466049fb5
unluckynilesh <nileshinde001@gmail.com>
2025-04-22 14:48:21 +0530

    fixxed issue

--- server/api/reddit_story.py
+++ server/api/reddit_story.py
@@ -72,18 +72,16 @@ async def create_reddit_post(
         caption = generate_caption(title)
         print("🧠 Caption generated:", caption)
 
-        task = create_reddit_post_task.apply_async(
-        kwargs={
-        "avatar_path": avatar_path,
-        "username": username,
-        "title": title,
-        "script": script,
-        "caption": caption,
-        "voice": voice,
-        "video": video,
-        "font": font,
-        "user_email": user_email
-        }
+        task = create_reddit_post_task.delay(
+          avatar_path,
+          username,
+           title,
+           script,
+           caption,
+           voice,
+           video,
+           font,
+           user_email
         )
 
         print("📦 Task queued:", task.id)
