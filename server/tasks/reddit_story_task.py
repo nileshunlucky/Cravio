e98b68c785408de54efb055982fd11d739c578b7
unluckynilesh <nileshinde001@gmail.com>
2025-04-25 16:08:51 +0530

    font small

--- server/tasks/reddit_story_task.py
+++ server/tasks/reddit_story_task.py
@@ -197,7 +197,7 @@ WrapStyle: 0
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Arial,180,&H00{bgr_code},&H000000FF,&H00000000,&H80000000,-1,0,0,0,100,100,0,0,1,2,0,5,10,10,10,1
+Style: Default,Arial,150,&H00{bgr_code},&H000000FF,&H00000000,&H80000000,-1,0,0,0,100,100,0,0,1,2,0,5,10,10,10,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
