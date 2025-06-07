60ac3fb9c21fcad139aeeea6d00b3541b77a98f3
unluckynilesh <nileshinde001@gmail.com>
2025-06-07 23:19:28 +0530

    trial upgrade 3

--- client/components/Offer.tsx
+++ client/components/Offer.tsx
@@ -173,6 +173,7 @@ const Offer = () => {
         }
     }, [])
 
+    if (!show) return null
 
     return (
         // Centering the component using fixed positioning and transform
@@ -280,20 +281,3 @@ const Offer = () => {
 }
 
 export default Offer
-
-
-
-
-    // < svg width = "20" height = "20" viewBox = "0 0 24 24" strokeWidth = "1.5" fill = "none" xmlns = "http://www.w3.org/2000/svg" className = "inline text-yellow-500" >
-    //                             <defs>
-    //                                 <linearGradient id="iconGradient" gradientTransform="rotate(90)">
-    //                                     <stop offset="0%" stopColor="#FFE629" />
-    //                                     <stop offset="100%" stopColor="#FFA057" />
-    //                                 </linearGradient>
-    //                             </defs>
-    //                             <path
-    //                                 d="M13.2319 2.28681C13.5409 2.38727 13.75 2.6752 13.75 3.00005V9.25005H19C19.2821 9.25005 19.5403 9.40834 19.6683 9.65972C19.7963 9.9111 19.7725 10.213 19.6066 10.4412L11.6066 21.4412C11.4155 21.7039 11.077 21.8137 10.7681 21.7133C10.4591 21.6128 10.25 21.3249 10.25 21.0001V14.7501H5C4.71791 14.7501 4.45967 14.5918 4.33167 14.3404C4.20366 14.089 4.22753 13.7871 4.39345 13.5589L12.3935 2.55892C12.5845 2.2962 12.923 2.18635 13.2319 2.28681Z"
-    //                                 fill="url(#iconGradient)"
-    //                                 stroke="currentColor"
-    //                             />
-    //                         </svg >
