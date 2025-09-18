e3a8026b68f0423c468c05bb8cd0e77ccbbbda26
unluckynilesh <nileshinde001@gmail.com>
2025-09-19 00:23:55 +0530

    clone higgsfieldai

--- client/app/admin/layout.tsx
+++ client/app/admin/layout.tsx
@@ -3,38 +3,15 @@
 import { SidebarProvider } from "@/components/ui/sidebar"
 import Nav from '@/components/Nav'
 import SendEmailToBackend from "@/components/SendEmailToBackend"
-import { SignedIn, useUser } from "@clerk/nextjs"
-import React, { useEffect, useState } from "react"
+import { SignedIn } from "@clerk/nextjs"
 import { Toaster } from "sonner";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { user } = useUser()
-  const [aura, setAura] = useState<number | null>(null)
-
-  useEffect(() => {
-    const fetchVideos = async () => {
-      if (!user?.primaryEmailAddress?.emailAddress) return
-      const email = user.primaryEmailAddress.emailAddress
-      
-
-      try {
-        const res = await fetch(`https://cravio-ai.onrender.com/user/${email}`)
-        const data = await res.json()
-        setAura(data.aura)
-      } catch (error) {
-        console.error('Error fetching videos:', error)
-      } 
-    }
-    
-    if (user) {
-      fetchVideos()
-    }
-  }, [user])
 
   return (
     <SidebarProvider>
       <div className="flex flex-col w-full">
-        <Nav aura={aura ?? 0} />
+        <Nav />
         <SignedIn>
           <SendEmailToBackend />
         </SignedIn>
