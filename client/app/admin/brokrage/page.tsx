9620a979c883c530150eb7dc0b2d14239a369e68
unluckynilesh <nileshinde001@gmail.com>
2025-12-11 17:10:16 +0530

    vercel error 4

--- client/app/admin/brokrage/page.tsx
+++ client/app/admin/brokrage/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
