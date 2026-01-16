4f3f9384cfc4456fb69935b1f83b526ad3454564
unluckynilesh <nileshinde001@gmail.com>
2026-01-16 13:06:22 +0530

    debug page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import Footer from '@/components/Footer'
-import Monitize from '@/components/Monitize'
 import Nav from '@/components/Nav'
 import Review from '@/components/Review'
 import FAQ from '@/components/FAQ'
-import HeroX from '@/components/HeroX'
 import Hero from '@/components/Hero'
 import React from 'react'
 import Anything from '@/components/Anything';
